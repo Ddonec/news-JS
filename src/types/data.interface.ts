0acfedd58073cbcaff7ca6a1368968c3e66b4985
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-26 11:12:48 +0300

    refactor: data.interfaces.ts rebuild with extends and acticles

--- src/types/data.interface.ts
+++ src/types/data.interface.ts
@@ -1,67 +1,34 @@
-// export interface NewsItem {
-//     author: string;
-//     name: string;
-//     id: string;
-//     title: string;
-//     description: string;
-//     url: string;
-//     publishedAt: string;
-//     urlToImage: string;
-//     source: { name: string; id: string };
-// }
-
-// export interface NewsData extends NewsItem {
-//     category: string;
-//     language: string;
-//     country: string;
-// }
-
-// export interface NewsSource extends NewsItem {
-//     status: string;
-//     sources: NewsData[];
-// }
-
 export interface NewsItem {
-    description: string;
-    author: string;
-    name: string;
     id: string;
-    title: string;
     url: string;
-    publishedAt: string;
     urlToImage: string;
+    name: string;
+    author: string;
+    publishedAt: string;
+    title: string;
+    description: string;
     source: { name: string; id: string };
 }
 
-export interface NewsData {
-    country?: string;
-    language?: string;
+export interface NewsData extends NewsItem {
     category?: string;
+    language?: string;
+    country?: string;
     articles?: Articles[];
-    description: string;
-    author: string;
-    name: string;
-    id: string;
-    title: string;
-    url: string;
-    publishedAt: string;
-    urlToImage: string;
-    source: { name: string; id: string };
 }
 
 export interface NewsSource {
     status: string;
     sources: NewsData[];
 }
-
 export interface Articles {
-    description: string;
-    author: string;
-    name: string;
     id: string;
-    title: string;
     url: string;
-    publishedAt: string;
     urlToImage: string;
+    author: string;
+    publishedAt: string;
+    name: string;
+    title: string;
+    description: string;
     source: { name: string; id: string };
 }
